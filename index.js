3f7c0b90c926abbdba7ade3640f6381f2580f791
Omid Kayhani <o.kayhani@gmail.com>
2023-04-13 20:21:45 +0300

    Done with the assignment.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+
+const random = Math.round(1 + 10 * Math.random());
+
+const num3 = 25;
+const num4 = 7;
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 3, 4, 20)
